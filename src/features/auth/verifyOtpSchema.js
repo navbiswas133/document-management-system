4a374bb7d58081e239a6943a3b5edd35cd92fe70
navkumar <navkumar.biswas@codenicely.in>
2026-08-09 07:40:58 +0530

    feat: validation for OTP form

--- src/features/auth/verifyOtpSchema.js
+++ src/features/auth/verifyOtpSchema.js
@@ -1,8 +1,5 @@
 import { z } from 'zod';
 
 export const verifyOtpSchema = z.object({
-  otp: z
-    .string()
-    .min(1, 'OTP is required')
-    .regex(/^\d+$/, 'Enter a valid OTP'),
+  otp: z.string().min(1, 'OTP is required'),
 });
